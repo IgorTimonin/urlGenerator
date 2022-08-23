4e14d5b8dc61e3cb32dfe4d5ae79b582bd9c5c74
IgorTimonin <mut4nt@yandex.ru>
2022-08-23 13:20:33 +0300

    added function to transliterate latin into cyrillic

--- urlGenerator.js
+++ urlGenerator.js
@@ -1,21 +1,24 @@
+const { transliterate } = require("./componets/transliterator");
+
 const urlGenerator = (string) => {
   const protocol = 'https://';
-  const subdomain = 'example'
+  const subdomain = 'example';
   const domain = 'mysite';
-  const zone = 'com'
-
+  const zone = 'com';
+  const port = '';
   const restrictedCharacters = 'àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;';
   const charactersReplacements = 'aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnooooooooprrsssssttuuuuuuuuuwxyyzzz------';
   const specialCharactersRegExp = new RegExp(restrictedCharacters.split('').join('|'), 'g');
 
   return `${protocol}${subdomain}.${domain}.${zone}/${
-    string
+    transliterate(string)
       .toString()
       .toLowerCase()
       .replace(/\s+/g, '-') // заменяем whitespace на -
       .replace(specialCharactersRegExp, (c) =>
         charactersReplacements.charAt(restrictedCharacters.indexOf(c))
       ) // заменяем специальные символы
+      .replace()
       .replace(/&/g, '-and-') // заменяем & на 'and'
       .replace(/[^\w\-]+/g, '') // убираем не слова
       .replace(/\-\-+/g, '-') // заменяем несколько - на один -
@@ -23,5 +26,5 @@ const urlGenerator = (string) => {
       .replace(/-+$/, '') // обрезаем - в конце строки
   }`;
 };
-
+console.log(urlGenerator('видео о непростой жизни Гималайской патагонской мары'));
 module.exports = urlGenerator;
