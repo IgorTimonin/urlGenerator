4d78de1721f544f1913d091a9de17290f03c6b1b
IgorTimonin <mut4nt@yandex.ru>
2022-08-23 12:23:03 +0300

    add const for url castomization

--- urlGenerator.js
+++ urlGenerator.js
@@ -1,17 +1,27 @@
 const urlGenerator = (string) => {
+  const protocol = 'http://';
+  const subdomain = 'example'
+  const domain = 'mysite';
+  const zone = 'ru'
+
   const restrictedCharacters = 'àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;';
   const charactersReplacements = 'aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnooooooooprrsssssttuuuuuuuuuwxyyzzz------';
   const specialCharactersRegExp = new RegExp(restrictedCharacters.split('').join('|'), 'g');
 
-  return `http://${string.toString().toLowerCase()
-    .replace(/\s+/g, '-') // заменяем whitespace на -
-    .replace(specialCharactersRegExp, c => charactersReplacements.charAt(restrictedCharacters.indexOf(c))) // заменяем специальные символы
-    .replace(/&/g, '-and-') // заменяем & на 'and'
-    .replace(/[^\w\-]+/g, '') // убираем не слова
-    .replace(/\-\-+/g, '-') // заменяем несколько - на один -
-    .replace(/^-+/, '') // обрезаем - в начале строки
-    .replace(/-+$/, '') // обрезаем - в конце строки
-  }`
+  return `${protocol}${subdomain}${domain}${zone}${
+    string
+      .toString()
+      .toLowerCase()
+      .replace(/\s+/g, '-') // заменяем whitespace на -
+      .replace(specialCharactersRegExp, (c) =>
+        charactersReplacements.charAt(restrictedCharacters.indexOf(c))
+      ) // заменяем специальные символы
+      .replace(/&/g, '-and-') // заменяем & на 'and'
+      .replace(/[^\w\-]+/g, '') // убираем не слова
+      .replace(/\-\-+/g, '-') // заменяем несколько - на один -
+      .replace(/^-+/, '') // обрезаем - в начале строки
+      .replace(/-+$/, '') // обрезаем - в конце строки
+  }`;
 };
 
 module.exports = urlGenerator;
