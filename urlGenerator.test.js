0a79f972ed7ccf1872df5955189f8bce0f036810
IgorTimonin <mut4nt@yandex.ru>
2022-08-23 13:35:20 +0300

    added .test that the result is a string

--- urlGenerator.test.js
+++ urlGenerator.test.js
@@ -1,10 +1,7 @@
 const urlGenerator= require('./urlGenerator.js');
 const urlRegEx = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/;
-console.log(urlGenerator('yrh6hv6rh7h7rjbr7jbr7j7rj  7h7h7j7j 7 '));
-it ('Генерирует URL-адрес', () => {
-    expect(
-    urlGenerator(
-        'àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;'
-    )
-    ).toMatch(urlRegEx);
+
+it ('generate url string', () => {
+    expect(typeof urlGenerator('řâñđöḿ text')).toBe('string');
+    expect(urlGenerator('řâñđöḿ text')).toMatch(urlRegEx);
 })
